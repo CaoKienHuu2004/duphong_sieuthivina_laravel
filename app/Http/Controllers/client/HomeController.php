a2afc3d7d2725547c9e2070192bcacfe9e9ebe62
Cao Kiến Hựu <ckienhuu12a12021@gmail.com>
2025-10-18 23:43:00 +0800

    up

--- app/Http/Controllers/client/HomeController.php
+++ app/Http/Controllers/client/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
+use App\Models\BientheModel;
 use App\Models\DanhmucModel;
 use App\Models\QuangcaoModel;
 use App\Models\QuatangsukienModel;
 use App\Models\SanPhamModel;
+use App\Models\ThuonghieuModel;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -17,13 +19,29 @@ class HomeController extends Controller
         $alldanhmuc = $this->alldanhmuc();
         $topdeals = $this->topdeals();
         $quatang = $this->quatang();
+        $topCategoriesResult = $this->danhmuchangdau(); 
+            $danhsachdmhangdau = $topCategoriesResult['danhsachdmhangdau']; 
+            $sanphamthuocdanhmuc = $topCategoriesResult['sanphamthuocdanhmuc'];
+        $topbrands = $this->thuonghieuhangdau();
+        $topproducts = $this->sanphamhangdau();
 
         // return response()->json([
         //     'banner' => $banner,
         //     'alldanhmuc' => $alldanhmuc,
         //     'topdeals' => $topdeals,
+        //     'quatang' => $quatang,
+        //     'danhsachdmhangdau' => $danhsachdmhangdau,
+        //     'sanphamthuocdanhmuc' => $sanphamthuocdanhmuc,
         // ]);
-        return view('client.index', compact('banner', 'alldanhmuc', 'topdeals', 'quatang')); 
+        return view('client.index', compact('banner',
+         'alldanhmuc',
+                    'topdeals',
+                    'quatang',
+                    'danhsachdmhangdau',
+                    'sanphamthuocdanhmuc',
+                    'topbrands',
+                    'topproducts',
+        )); 
     }
 
     protected function banner()
@@ -46,7 +64,7 @@ class HomeController extends Controller
 
     protected function topdeals()
     {
-        $topdeals = SanPhamModel::where('giamgia', '>', 0) // Chỉ lấy sản phẩm có giảm giá
+        $topdeals = SanPhamModel::where('giamgia', '>', 0)
             ->where('trangthai', 'Công khai')
             ->with(['hinhanhsanpham', 'thuonghieu', 'danhmuc', 'bienthe'])
             ->withSum('bienthe', 'luotban')
@@ -79,5 +97,114 @@ class HomeController extends Controller
                                 ->orderBy('luotxem', 'desc')
                                 ->get();
         return $quatang;
+    }
+    protected function danhmuchangdau()
+    {
+        $topCategorySales = BientheModel::whereIn('bienthe.trangthai', ['Còn hàng', 'Sắp hết hàng'])
+                                        ->whereNull('bienthe.deleted_at')
+                                        ->join('sanpham', 'bienthe.id_sanpham', '=', 'sanpham.id') 
+                                        ->join('danhmuc_sanpham', 'sanpham.id', '=', 'danhmuc_sanpham.id_sanpham') 
+                                        ->select('danhmuc_sanpham.id_danhmuc')
+                                        ->selectRaw('SUM(bienthe.luotban) as total_sales')
+                                        ->groupBy('danhmuc_sanpham.id_danhmuc')
+                                        ->orderByDesc('total_sales')
+                                        ->limit(5)
+                                        ->get();
+
+        $topCategoryIds = $topCategorySales->pluck('id_danhmuc')->toArray();
+        
+        $topCategoriesList = DanhmucModel::whereIn('id', $topCategoryIds)
+                                            ->get()
+                                            ->sortBy(function($category) use ($topCategorySales) {
+                                                $salesData = $topCategorySales->firstWhere('id_danhmuc', $category->id);
+                                                return $salesData ? $salesData->total_sales : 0;
+                                            }, SORT_REGULAR, true);
+
+        $topProductsByCategory = collect();
+        
+        foreach ($topCategoryIds as $categoryId) {
+            $products = SanphamModel::with(['hinhanhsanpham', 'thuonghieu', 'danhmuc', 'bienthe'])
+                                    ->whereHas('danhmuc', function ($query) use ($categoryId) {
+                                        $query->where('id_danhmuc', $categoryId);
+                                    })
+                                    ->join('bienthe', 'sanpham.id', '=', 'bienthe.id_sanpham') 
+                                    ->select('sanpham.*')
+                                    ->selectRaw('SUM(bienthe.luotban) as product_total_sales')
+                                    ->whereIn('bienthe.trangthai', ['Còn hàng', 'Sắp hết hàng']) 
+                                    ->whereNull('sanpham.deleted_at') 
+                                    ->whereNull('bienthe.deleted_at') 
+                                    ->groupBy('sanpham.id')
+                                    ->orderByDesc('product_total_sales')
+                                    ->limit(12)
+                                    ->get()
+                                    ->tap(function ($collection) {
+                                        $collection->each(function ($sanpham) {
+                                            if ($sanpham->bienthe->isNotEmpty()) {
+                                                $cheapestVariant = $sanpham->bienthe->sortBy('giagoc')->first();
+                                                $sanpham->bienthe = $cheapestVariant;
+                                                $giagoc = $cheapestVariant->giagoc;
+                                                $giamgiaPercent = $sanpham->giamgia / 100;
+                                                $sanpham->giadagiam = $giagoc * (1 - $giamgiaPercent);
+                                            } else {
+                                                $sanpham->bienthe = null;
+                                                $sanpham->giadagiam = null;
+                                            }
+                                        });
+                                    });
+
+            $topProductsByCategory->put($categoryId, $products);
+        }
+
+        return [
+            'danhsachdmhangdau' => $topCategoriesList, 
+            'sanphamthuocdanhmuc' => $topProductsByCategory, 
+        ];
+    }
+    protected function thuonghieuhangdau(){
+        $topBrandSales = BientheModel::whereIn('bienthe.trangthai', ['Còn hàng', 'Sắp hết hàng'])
+            ->whereNull('bienthe.deleted_at')
+            ->join('sanpham', 'bienthe.id_sanpham', '=', 'sanpham.id')
+            ->join('thuonghieu', 'sanpham.id_thuonghieu', '=', 'thuonghieu.id')
+            ->select('thuonghieu.id')
+            ->selectRaw('SUM(bienthe.luotban) as total_sales')
+            ->groupBy('thuonghieu.id')
+            ->orderByDesc('total_sales')
+            ->limit(5)
+            ->get();
+
+        $topBrandIds = $topBrandSales->pluck('id')->toArray();
+
+        $topBrands = ThuonghieuModel::whereIn('id', $topBrandIds)
+            ->get()
+            ->sortBy(function ($brand) use ($topBrandSales) {
+                $salesData = $topBrandSales->firstWhere('id', $brand->id);
+                return $salesData ? $salesData->total_sales : 0;
+            }, SORT_REGULAR, true);
+
+        return $topBrands;
+    }
+    protected function sanphamhangdau()
+    {
+        $topProducts = SanPhamModel::where('trangthai', 'Công khai')
+            ->with(['hinhanhsanpham', 'thuonghieu', 'danhmuc', 'bienthe'])
+            ->withSum('bienthe', 'luotban')
+            ->orderBy('bienthe_sum_luotban', 'desc')
+            ->limit(10)
+            ->get()
+            ->tap(function ($collection) {
+                $collection->each(function ($sanpham) {
+                    if ($sanpham->bienthe->isNotEmpty()) {
+                        $cheapestVariant = $sanpham->bienthe->sortBy('giagoc')->first();
+                        $sanpham->bienthe = $cheapestVariant;
+                        $giagoc = $cheapestVariant->giagoc;
+                        $giamgiaPercent = $sanpham->giamgia / 100;
+                        $sanpham->giadagiam = $giagoc * (1 - $giamgiaPercent);
+                    } else {
+                        $sanpham->bienthe = null;
+                        $sanpham->giadagiam = null;
+                    }
+                });
+            });
+        return $topProducts;
     }
 }
--- sieuthivina.sql
+++ sieuthivina.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 16, 2025 at 10:55 AM
+-- Generation Time: Oct 18, 2025 at 03:40 PM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.16
 
@@ -63,12 +63,18 @@ CREATE TABLE `bienthe` (
 INSERT INTO `bienthe` (`id`, `id_loaibienthe`, `id_sanpham`, `giagoc`, `soluong`, `luottang`, `luotban`, `trangthai`, `deleted_at`) VALUES
 (1, 1, 1, 270000, 10, 0, 0, 'Còn hàng', NULL),
 (2, 2, 2, 385000, 10, 0, 0, 'Còn hàng', NULL),
-(3, 1, 3, 466560, 10, 0, 0, 'Còn hàng', NULL),
+(3, 1, 3, 466560, 10, 0, 23, 'Còn hàng', NULL),
 (4, 1, 4, 260000, 10, 0, 0, 'Còn hàng', NULL),
 (5, 2, 5, 512000, 10, 0, 0, 'Còn hàng', NULL),
 (6, 1, 6, 270000, 2, 0, 0, 'Còn hàng', NULL),
-(7, 2, 9, 360000, 253, 0, 0, 'Còn hàng', NULL),
-(8, 1, 9, 260000, 5, 0, 0, 'Còn hàng', NULL);
+(7, 2, 9, 360000, 253, 0, 28, 'Còn hàng', NULL),
+(8, 1, 9, 260000, 5, 0, 2, 'Còn hàng', NULL),
+(9, 1, 10, 795000, 200, 0, 10, 'Còn hàng', NULL),
+(10, 2, 11, 950000, 27, 0, 124, 'Còn hàng', NULL),
+(11, 1, 11, 500000, 5, 0, 10, 'Sắp hết hàng', NULL),
+(12, 1, 12, 330000, 63, 0, 12, 'Còn hàng', NULL),
+(13, 2, 12, 330000, 92, 0, 72, 'Còn hàng', NULL),
+(14, 3, 12, 512000, 12, 0, 0, 'Còn hàng', NULL);
 
 -- --------------------------------------------------------
 
@@ -118,7 +124,7 @@ CREATE TABLE `danhgia` (
   `id_sanpham` int NOT NULL,
   `id_nguoidung` int NOT NULL,
   `id_chitietdonhang` int NOT NULL,
-  `diem` int NOT NULL,
+  `diem` int NOT NULL DEFAULT '5',
   `noidung` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `trangthai` enum('Hiển thị','Tạm ẩn') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Hiển thị'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -134,25 +140,26 @@ CREATE TABLE `danhmuc` (
   `ten` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `slug` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `logo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'danhmuc.jpg',
-  `parent` enum('Cha','Con') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Cha'
+  `parent` enum('Cha','Con') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Cha',
+  `trangthai` enum('Hiển thị','Tạm ẩn') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Hiển thị'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `danhmuc`
 --
 
-INSERT INTO `danhmuc` (`id`, `ten`, `slug`, `logo`, `parent`) VALUES
-(1, 'Sức khỏe', 'suc-khoe', 'suc-khoe.svg', 'Cha'),
-(2, 'Thực phẩm chức năng', 'thuc-pham-chuc-nang', 'thuc-pham-chuc-nang.svg', 'Cha'),
-(3, 'Chăm sóc cá nhân', 'cham-soc-ca-nhan', 'cham-soc-ca-nhan.svg', 'Cha'),
-(4, 'Làm đẹp', 'lam-dep', 'lam-dep.svg', 'Cha'),
-(5, 'Điện máy', 'dien-may', 'dien-may.svg', 'Cha'),
-(6, 'Thiết bị y tế', 'thiet-bi-y-te', 'thiet-bi-y-te.svg', 'Cha'),
-(7, 'Bách hóa', 'bach-hoa', 'bach-hoa.svg', 'Cha'),
-(8, 'Nội thất - Trang trí', 'noi-that-trang-tri', 'noi-that-trang-tri.svg', 'Cha'),
-(9, 'Mẹ & bé', 'me-va-be', 'me-va-be.svg', 'Cha'),
-(10, 'Thời trang', 'thoi-trang', 'thoi-trang.svg', 'Cha'),
-(11, 'Thực phẩm - đồ ăn', 'thuc-pham-do-an', 'thuc-pham-do-an.svg', 'Cha');
+INSERT INTO `danhmuc` (`id`, `ten`, `slug`, `logo`, `parent`, `trangthai`) VALUES
+(1, 'Sức khỏe', 'suc-khoe', 'suc-khoe.svg', 'Cha', 'Hiển thị'),
+(2, 'Thực phẩm chức năng', 'thuc-pham-chuc-nang', 'thuc-pham-chuc-nang.svg', 'Cha', 'Hiển thị'),
+(3, 'Chăm sóc cá nhân', 'cham-soc-ca-nhan', 'cham-soc-ca-nhan.svg', 'Cha', 'Hiển thị'),
+(4, 'Làm đẹp', 'lam-dep', 'lam-dep.svg', 'Cha', 'Hiển thị'),
+(5, 'Điện máy', 'dien-may', 'dien-may.svg', 'Cha', 'Hiển thị'),
+(6, 'Thiết bị y tế', 'thiet-bi-y-te', 'thiet-bi-y-te.svg', 'Cha', 'Hiển thị'),
+(7, 'Bách hóa', 'bach-hoa', 'bach-hoa.svg', 'Cha', 'Hiển thị'),
+(8, 'Nội thất - Trang trí', 'noi-that-trang-tri', 'noi-that-trang-tri.svg', 'Cha', 'Hiển thị'),
+(9, 'Mẹ & bé', 'me-va-be', 'me-va-be.svg', 'Cha', 'Hiển thị'),
+(10, 'Thời trang', 'thoi-trang', 'thoi-trang.svg', 'Cha', 'Hiển thị'),
+(11, 'Thực phẩm - đồ ăn', 'thuc-pham-do-an', 'thuc-pham-do-an.svg', 'Cha', 'Hiển thị');
 
 -- --------------------------------------------------------
 
@@ -175,7 +182,15 @@ INSERT INTO `danhmuc_sanpham` (`id`, `id_danhmuc`, `id_sanpham`) VALUES
 (2, 1, 4),
 (3, 2, 1),
 (4, 2, 3),
-(5, 1, 2);
+(5, 1, 2),
+(6, 2, 9),
+(7, 3, 9),
+(8, 1, 9),
+(9, 4, 6),
+(10, 1, 10),
+(11, 4, 10),
+(12, 4, 11),
+(13, 4, 12);
 
 -- --------------------------------------------------------
 
@@ -189,7 +204,7 @@ CREATE TABLE `diachi_nguoidung` (
   `hoten` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `sodienthoai` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `diachi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `trangthai` enum('Mặc định','Khác','Tạm ẩn') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `trangthai` enum('Mặc định','Khác','Tạm ẩn') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Khác',
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -274,7 +289,20 @@ INSERT INTO `hinhanh_sanpham` (`id`, `id_sanpham`, `hinhanh`, `trangthai`, `dele
 (24, 9, 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg-2.webp', 'Hiển thị', NULL),
 (25, 9, 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg-3.webp', 'Hiển thị', NULL),
 (26, 9, 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg-4.webp', 'Hiển thị', NULL),
-(27, 9, 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg-5.webp', 'Hiển thị', NULL);
+(27, 9, 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg-5.webp', 'Hiển thị', NULL),
+(28, 10, 'collagen-thuy-phan-ho-tro-da-mong-toc-acai-labs-marine-collagen-beauty-australia-90v-1.webp', 'Hiển thị', NULL),
+(29, 10, 'collagen-thuy-phan-ho-tro-da-mong-toc-acai-labs-marine-collagen-beauty-australia-90v-2.webp', 'Hiển thị', NULL),
+(30, 10, 'collagen-thuy-phan-ho-tro-da-mong-toc-acai-labs-marine-collagen-beauty-australia-90v-3.webp', 'Hiển thị', NULL),
+(31, 10, 'collagen-thuy-phan-ho-tro-da-mong-toc-acai-labs-marine-collagen-beauty-australia-90v-4.webp', 'Hiển thị', NULL),
+(32, 10, 'collagen-thuy-phan-ho-tro-da-mong-toc-acai-labs-marine-collagen-beauty-australia-90v-5.webp', 'Hiển thị', NULL),
+(33, 11, 'vien-uong-bishin-tripeptide-collagen-nhat-ban-60v-1.webp', 'Hiển thị', NULL),
+(34, 11, 'vien-uong-bishin-tripeptide-collagen-nhat-ban-60v-2.webp', 'Hiển thị', NULL),
+(35, 11, 'vien-uong-bishin-tripeptide-collagen-nhat-ban-60v-3.webp', 'Hiển thị', NULL),
+(36, 11, 'vien-uong-bishin-tripeptide-collagen-nhat-ban-60v-4.webp', 'Hiển thị', NULL),
+(37, 12, 'duong-mi-te-bao-goc-cchoi-bio-placenta-lash-serum-1.webp', 'Hiển thị', NULL),
+(38, 12, 'duong-mi-te-bao-goc-cchoi-bio-placenta-lash-serum-2.webp', 'Hiển thị', NULL),
+(39, 12, 'duong-mi-te-bao-goc-cchoi-bio-placenta-lash-serum-3.webp', 'Hiển thị', NULL),
+(40, 12, 'duong-mi-te-bao-goc-cchoi-bio-placenta-lash-serum-4.webp', 'Hiển thị', NULL);
 
 -- --------------------------------------------------------
 
@@ -294,7 +322,8 @@ CREATE TABLE `loaibienthe` (
 
 INSERT INTO `loaibienthe` (`id`, `ten`, `trangthai`) VALUES
 (1, 'Lọ', 'Hiển thị'),
-(2, 'Hộp', 'Hiển thị');
+(2, 'Hộp', 'Hiển thị'),
+(3, 'Gói', 'Hiển thị');
 
 -- --------------------------------------------------------
 
@@ -396,7 +425,7 @@ INSERT INTO `quangcao` (`id`, `vitri`, `hinhanh`, `lienket`, `mota`, `trangthai`
 CREATE TABLE `quatang_sukien` (
   `id` int NOT NULL,
   `id_bienthe` int NOT NULL,
-  `id_sukien` int NOT NULL,
+  `id_chuongtrinh` int NOT NULL,
   `dieukien` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tieude` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `thongtin` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -412,8 +441,9 @@ CREATE TABLE `quatang_sukien` (
 -- Dumping data for table `quatang_sukien`
 --
 
-INSERT INTO `quatang_sukien` (`id`, `id_bienthe`, `id_sukien`, `dieukien`, `tieude`, `thongtin`, `hinhanh`, `luotxem`, `ngaybatdau`, `ngayketthuc`, `trangthai`, `deleted_at`) VALUES
-(1, 1, 1, '2', 'Ưu đãi sinh nhật 13/10 - Tặng 1 sản phẩm bất kỳ', 'Mua 2 sản phẩm từ Trung Tâm Bán Hàng Siêu Thị Vina để nhận được ưu đãi tặng 1 sản phẩm nhân ngày sinh nhật 13/10', 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg-2.webp', 0, '2025-10-15', '2025-12-01', 'Hiển thị', NULL);
+INSERT INTO `quatang_sukien` (`id`, `id_bienthe`, `id_chuongtrinh`, `dieukien`, `tieude`, `thongtin`, `hinhanh`, `luotxem`, `ngaybatdau`, `ngayketthuc`, `trangthai`, `deleted_at`) VALUES
+(1, 1, 1, '2', 'Ưu đãi sinh nhật 13/10 - Tặng 1 sản phẩm bất kỳ', 'Mua 2 sản phẩm từ Trung Tâm Bán Hàng Siêu Thị Vina để nhận được ưu đãi tặng 1 sản phẩm nhân ngày sinh nhật 13/10', 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg-2.webp', 0, '2025-10-18', '2025-11-02', 'Hiển thị', NULL),
+(2, 2, 1, '5', 'Tặng 1 sản phẩm từ thương hiệu khi thêm 5 sản phẩm bất kỳ trong giỏ hàng của thương hiệu', 'Không có thông tin', 'sam-ngoc-linh-truong-sinh-do-thung-24lon-1.webp', 5, '2025-10-18', '2025-10-25', 'Hiển thị', NULL);
 
 -- --------------------------------------------------------
 
@@ -442,11 +472,14 @@ CREATE TABLE `sanpham` (
 INSERT INTO `sanpham` (`id`, `id_thuonghieu`, `ten`, `slug`, `mota`, `xuatxu`, `sanxuat`, `trangthai`, `giamgia`, `luotxem`, `deleted_at`) VALUES
 (1, 1, 'Keo ong xanh Tracybee Propolis Mint & Honey – Giảm đau rát họng, ho, viêm họng (Vị Bạc Hà)', 'keo-ong-xanh-tracybee-propolis-mint-honey-giam-dau-rat-hong-ho-viem-hong-vi-bac-ha', 'Bạn đang tìm kiếm giải pháp kháng khuẩn tự nhiên và giảm đau họng tức thì? Keo ong xanh Tracybee Propolis Mint & Honey là sự kết hợp hoàn hảo giữa dược liệu quý từ thiên nhiên và hương vị the mát, giúp bạn vượt qua những cơn đau rát họng, ho và viêm họng khó chịu một cách nhanh chóng.', 'Brazil', 'Nhập khẩu chính ngạch bởi Siêu Thị Vina', 'Công khai', 10, 0, NULL),
 (2, 1, 'Mật ong Tây Bắc đông trùng hạ thảo X3 (Hũ 240g)', 'mat-ong-tay-bac-dong-trung-ha-thao-x3-hu-240g', 'Mật ong Tây Bắc Đông Trùng Hạ Thảo X3 là siêu phẩm bồi bổ sức khỏe, kết hợp giữa mật ong rừng Tây Bắc nguyên chất và hàm lượng Đông Trùng Hạ Thảo được tăng cường gấp 3 lần (X3). Sản phẩm mang đến giải pháp tiện lợi và hiệu quả tối ưu để nâng cao thể trạng, tăng cường đề kháng và phục hồi sức khỏe.', 'Việt Nam', 'Việt Nam', 'Công khai', 20, 0, NULL),
-(3, 1, 'Sâm Ngọc Linh trường sinh đỏ (Thùng 24lon)', 'sam-ngoc-linh-truong-sinh-do-thung-24lon', 'Sâm Ngọc Linh Trường Sinh Đỏ là tinh hoa của dược liệu quý hiếm, mang đến giải pháp tiện lợi để bồi bổ sức khỏe và nâng cao thể trạng mỗi ngày. Được chiết xuất từ Sâm Ngọc Linh quý giá – \"Quốc bảo của Việt Nam\" – sản phẩm ở dạng lon uống liền giúp bạn hấp thu trọn vẹn dưỡng chất một cách nhanh chóng và hiệu quả.', 'Việt Nam', 'Việt Nam', 'Công khai', 10, 0, NULL),
+(3, 1, 'Sâm Ngọc Linh trường sinh đỏ (Thùng 24lon)', 'sam-ngoc-linh-truong-sinh-do-thung-24lon', 'Sâm Ngọc Linh Trường Sinh Đỏ là tinh hoa của dược liệu quý hiếm, mang đến giải pháp tiện lợi để bồi bổ sức khỏe và nâng cao thể trạng mỗi ngày. Được chiết xuất từ Sâm Ngọc Linh quý giá – \"Quốc bảo của Việt Nam\" – sản phẩm ở dạng lon uống liền giúp bạn hấp thu trọn vẹn dưỡng chất một cách nhanh chóng và hiệu quả.', 'Việt Nam', 'Việt Nam', 'Công khai', 0, 0, NULL),
 (4, 1, 'Tinh dầu tràm tự nhiên ECO - Hỗ trợ giảm ho, cảm cúm, sổ mũi, cảm lạnh (Lọ 30ml)', 'tinh-dau-tram-tu-nhien-eco-ho-tro-giam-ho-cam-cum-so-mui-cam-lanh-lo-30ml', 'Tinh Dầu Tràm Tự Nhiên ECO là sản phẩm chiết xuất 100% từ lá tràm nguyên chất, mang trong mình những công dụng truyền thống tuyệt vời trong việc bảo vệ sức khỏe, đặc biệt là hệ hô hấp. Với khả năng kháng khuẩn, làm ấm và thư giãn, Tinh dầu Tràm ECO là người bạn đồng hành không thể thiếu trong tủ thuốc gia đình, giúp đối phó hiệu quả với các triệu chứng cảm thông thường.', 'Việt Nam', 'Việt Nam', 'Công khai', 85, 0, NULL),
 (5, 1, 'Sữa non tổ yến Papamilk Height & Gain giúp tăng cân tăng chiều cao cho Trẻ từ 1-19 tuổi - Lon 830G', 'sua-non-to-yen-papamilk-height-gain-giup-tang-can-tang-chieu-cao-cho-tre-tu-1-19-tuoi-lon-830g', 'Sữa Non Tổ Yến Papamilk Height & Gain là công thức dinh dưỡng đột phá được thiết kế chuyên biệt để hỗ trợ tăng cân khỏe mạnh và tối ưu hóa chiều cao cho trẻ em và thanh thiếu niên từ 1 đến 19 tuổi. Sự kết hợp độc đáo giữa Sữa Non cao cấp, Tổ Yến quý giá cùng hệ dưỡng chất khoa học giúp con bạn xây dựng nền tảng vững chắc cho một tương lai phát triển vượt trội.', 'Việt Nam', 'Việt Nam', 'Công khai', 20, 0, NULL),
 (6, 1, 'hahaha', 'hahahaha', 'ádasdasd', 'ss', 'ss', 'Công khai', 20, 1, NULL),
-(9, 1, 'Thực phẩm bảo vệ sức khỏe: Midu MenaQ7 180mcg', 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg', 'Midu MenaQ7 180mcg bổ sung canxi, Vitamin D3, Vitamin K2 dạng MenaQ7 và Arginine phù hợp với tất cả độ tuổi từ 1 đến 100 tuổi. Đặc biệt giúp phát triển chiều cao cho trẻ em 1-15 tuổi; mẹ bầu bổ sung canxi trong giai đoạn thai kì không gây tiểu đường, không gây táo bón và giúp con cao ngay từ trong bụng mẹ.', 'Việt Nam', 'Việt Nam', 'Công khai', 0, 23, NULL);
+(9, 1, 'Thực phẩm bảo vệ sức khỏe: Midu MenaQ7 180mcg', 'thuc-pham-bao-ve-suc-khoe-midu-menaq7-180mcg', 'Midu MenaQ7 180mcg bổ sung canxi, Vitamin D3, Vitamin K2 dạng MenaQ7 và Arginine phù hợp với tất cả độ tuổi từ 1 đến 100 tuổi. Đặc biệt giúp phát triển chiều cao cho trẻ em 1-15 tuổi; mẹ bầu bổ sung canxi trong giai đoạn thai kì không gây tiểu đường, không gây táo bón và giúp con cao ngay từ trong bụng mẹ.', 'Việt Nam', 'Việt Nam', 'Công khai', 10, 23, NULL),
+(10, 3, 'Collagen thủy phân hỗ trợ Da Móng Tóc Acai Labs Marine Collagen Beauty Australia 90v', 'collagen-thuy-phan-ho-tro-da-mong-toc-acai-labs-marine-collagen-beauty-australia-90v', 'Acacia Labs, với trụ sở chính tại Sydney, Australia, là thương hiệu hàng đầu trong lĩnh vực sản xuất thực phẩm chức năng, vitamin và khoáng chất. Chúng tôi tập trung vào việc nghiên cứu và phát triển các sản phẩm chăm sóc sức khỏe và sắc đẹp, mang đến cho khách hàng những giải pháp toàn diện cho một cuộc sống khỏe mạnh, tràn đầy năng lượng và hạnh phúc.\r\n\r\nKhởi nguồn từ vẻ đẹp thật sự đến từ bên trong, phòng thí nghiệm Acaci Labs với đội ngũ các chuyên gia hàng đầu thế giới đã không ngừng nghiên cứu tìm ra công thức và sản phẩm chất lượng tốt nhất để mang đến hạnh phúc lâu dài cho người sử dụng.', 'Australia', 'Australia', 'Công khai', 0, 67, NULL),
+(11, 1, 'Viên uống Bishin Tripeptide Collagen Nhật Bản 60v', 'vien-uong-bishin-tripeptide-collagen-nhat-ban-60v', 'BISHIN chứa tinh chất Collagen tripeptide giúp tăng cường khả năng hấp thụ cho cơ thể, cấu trúc da, giúp da giữ được sự săn chắc, đàn hồi, tươi trẻ, hạn chế được các nếp nhăn (dấu hiệu của sự lão hóa) xuất hiện.', 'Nhật Bản', 'Phân phối tại Việt Nam', 'Công khai', 0, 2, NULL),
+(12, 2, 'Dưỡng mi tế bào gốc C’Choi - Bio Placenta Lash Serum', 'duong-mi-te-bao-goc-cchoi-bio-placenta-lash-serum', 'DƯỠNG MI TẾ BÀO GỐC C’CHOI Bio-Placenta Lash Serum', 'Việt Nam', 'Việt Nam', 'Công khai', 30, 23, NULL);
 
 -- --------------------------------------------------------
 
@@ -482,7 +515,9 @@ CREATE TABLE `thuonghieu` (
 --
 
 INSERT INTO `thuonghieu` (`id`, `ten`, `slug`, `logo`, `trangthai`) VALUES
-(1, 'Trung Tâm Bán Hàng Siêu Thị Vina', 'trung-tam-ban-hang-sieu-thi-vina', 'logo_shop.jpg', 'Hoạt động');
+(1, 'Trung Tâm Bán Hàng Siêu Thị Vina', 'trung-tam-ban-hang-sieu-thi-vina', 'trung-tam-ban-hang-sieu-thi-vina.png', 'Hoạt động'),
+(2, 'C\'CHOI', 'cchoi', 'thuonghieu-logo.png', 'Hoạt động'),
+(3, 'ACACI LABS', 'acaci-labs', 'thuonghieu-logo.png', 'Hoạt động');
 
 -- --------------------------------------------------------
 
@@ -650,7 +685,7 @@ ALTER TABLE `quangcao`
 ALTER TABLE `quatang_sukien`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_bienthe` (`id_bienthe`),
-  ADD KEY `id_sukien` (`id_sukien`);
+  ADD KEY `id_sukien` (`id_chuongtrinh`);
 
 --
 -- Indexes for table `sanpham`
@@ -694,7 +729,7 @@ ALTER TABLE `yeuthich`
 -- AUTO_INCREMENT for table `bienthe`
 --
 ALTER TABLE `bienthe`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `chuongtrinh`
@@ -712,19 +747,19 @@ ALTER TABLE `danhmuc`
 -- AUTO_INCREMENT for table `danhmuc_sanpham`
 --
 ALTER TABLE `danhmuc_sanpham`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `hinhanh_sanpham`
 --
 ALTER TABLE `hinhanh_sanpham`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `loaibienthe`
 --
 ALTER TABLE `loaibienthe`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `nguoidung`
@@ -742,19 +777,19 @@ ALTER TABLE `quangcao`
 -- AUTO_INCREMENT for table `quatang_sukien`
 --
 ALTER TABLE `quatang_sukien`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `thuonghieu`
 --
 ALTER TABLE `thuonghieu`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tukhoa`
@@ -834,7 +869,7 @@ ALTER TABLE `hinhanh_sanpham`
 --
 ALTER TABLE `quatang_sukien`
   ADD CONSTRAINT `quatang_sukien_ibfk_1` FOREIGN KEY (`id_bienthe`) REFERENCES `bienthe` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `quatang_sukien_ibfk_3` FOREIGN KEY (`id_sukien`) REFERENCES `chuongtrinh` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `quatang_sukien_ibfk_3` FOREIGN KEY (`id_chuongtrinh`) REFERENCES `chuongtrinh` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `sanpham`
