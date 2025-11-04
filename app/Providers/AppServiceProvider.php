bf0c5cdf081a1cbab2daca37ef1d23ba35ff8eab
Cao Kiến Hựu <ckienhuu12a12021@gmail.com>
2025-11-05 00:50:45 +0800

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {   
-        Livewire::component('giohangComponent', \App\Livewire\GiohangComponent::class);
+        Livewire::component('giohang', \App\Livewire\GiohangComponent::class);
     }
 }
