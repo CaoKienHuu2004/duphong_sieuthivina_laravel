5ecb713bed2a8f5a07be431cb883504251c5ad54
Cao Kiến Hựu <ckienhuu12a12021@gmail.com>
2025-11-06 17:21:05 +0800

    Update giohang.blade.php

--- resources/views/client/thanhtoan/giohang.blade.php
+++ resources/views/client/thanhtoan/giohang.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="page">
          <section class="cart py-40">
-            @livewire('giohangComponent')
+            @livewire('GiohangComponent')
         </section>
     </div>
 @endsection
