baad2a0ec12700b844b4af7fa02178bc240b1475
Cao Kiến Hựu <ckienhuu12a12021@gmail.com>
2025-12-20 19:44:14 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->statefulApi();
         $middleware->validateCsrfTokens(except: [
-            'api/v1/dang-ky', 
+            'api/v1/dang-ky', 'api/v1/dang-nhap',
         ]);
         //
         $middleware->alias([
